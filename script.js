504df225e726eb94798798f19bff1a3191c56edb
Jann Marc Villablanca <jmfvillablanca@gmail.com>
2022-10-06 18:41:42 +0800

    Create player choice function

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-
+const rock = 'Rock';
+const paper = 'Paper';
+const scissors = 'Scissors';
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random()*3);
     if (compChoice === 0){
-         return 'Rock'; 
+         return rock; 
     }
     else if (compChoice === 1){ 
-         return 'Paper'; 
+         return paper; 
     }
     else{
-         return 'Scissors';
+         return scissors;
     }
 }
 
+function getPlayerChoice(){                             //No error handling, assume user enters valid values
+    let playerInput = prompt("Rock, Paper, Scissors?");
+    playerInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1,playerInput.length).toLowerCase(); 
+    return playerInput 
+}
+
+
+console.log(`Player Choice: ${playerSelection}, Computer Choice: ${computerSelection}`);
