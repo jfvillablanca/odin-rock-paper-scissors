f5fa540c89dd5bc1209f60dc193953d7ad6406ff
Jann Marc Villablanca <jmfvillablanca@gmail.com>
2022-10-14 20:44:42 +0800

    Clean code and remove console diagnostics

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
-let playerScore = 0, computerScore = 0, roundCounter = 0, noOfGameRounds = 5;
+let playerScore = 0, computerScore = 0, roundCounter = 1, noOfGameRounds = 5;
 
 function playRound(playerSelection, computerSelection) {
-    //console.log(`Player Choice: ${playerSelection}, Computer Choice: ${computerSelection}`);
     if ((playerSelection === rock && computerSelection === scissors) ||
         (playerSelection === paper && computerSelection === rock) ||
         (playerSelection === scissors && computerSelection === paper)) {
@@ -40,51 +39,42 @@ const game = function(playerChoice){
     const computerScoreDisplay = document.querySelector('#computerScore');
     const gameStatus = document.querySelector('.status');
     const roundDisplay = document.querySelector('#roundCycle');
+    const newGameButton = document.querySelector('.newGame');
 
     let winner = playRound(playerSelection, computerSelection);
     if (winner === 'Player') {
         playerScore++;
-//        console.log(`You win!`);
         playerScoreDisplay.textContent = `Player: ${playerScore}`;
         gameStatus.textContent = `You win against the computer!`;
     }
     else if (winner === 'Computer') {
         computerScore++;
-//        console.log("Computer wins!");
         computerScoreDisplay.textContent = `Computer: ${computerScore}`;
         gameStatus.textContent = `You lost against the computer!`;
     }
     else {
-//       console.log("It's a tie");
         gameStatus.textContent = `It's a tie!`;
     }
-    if (roundCounter >= noOfGameRounds){
+    if (roundCounter === noOfGameRounds) {
         if (playerScore > computerScore) {
-            //console.log(`For a final score of ${playerScore} against the computer's ${computerScore}, the player wins the match!`);
             gameStatus.textContent = `For a final score of ${playerScore} against the computer's ${computerScore}, the player wins the match!`;
         }
         else if (playerScore < computerScore) {
-            //console.log(`For a final score of ${computerScore} against the player's ${playerScore}, the computer wins the match!`);
             gameStatus.textContent = `For a final score of ${computerScore} against the player's ${playerScore}, the computer wins the match!`;
         }
         else {
-            //console.log(`The match is tied for a final score of ${playerScore} to ${computerScore} to both competitors!`);
             gameStatus.textContent = `The match is tied for a final score of ${playerScore} to ${computerScore} to both competitors!`
         }
-    newGame();
-    
-//    const divContainer = document.querySelector('.container');
-//    const newGameButton = document.createElement('button');
-//    newGameButton.textContent = "New Game?";
-//    divContainer.appendChild(newGameButton);
-//    newGameButton.addEventListener('click',newGame);
+        newGameButton.style.visibility = "visible";
+        newGameButton.removeAttribute("hidden");
+        newGameButton.addEventListener('click', newGame);
     }
-    roundCounter++;
+    if (roundCounter < noOfGameRounds) roundCounter++;
     roundDisplay.textContent = `Round: ${roundCounter}`;
 }
 
 const newGame = function(){
-    playerScore = 0, computerScore = 0, roundCounter = 0;
+    playerScore = 0, computerScore = 0, roundCounter = 1;
     console.trace();
     const divContainer = document.querySelector('.container');
     const playerScoreDisplay = document.querySelector('#playerScore');
@@ -104,9 +94,5 @@ const newGame = function(){
     buttons.forEach((button) => {
         button.addEventListener('click', game);
         });
-
-    newGameButton.style.visibility = "visible";
-    newGameButton.addEventListener('click',newGame);
 }
-
 newGame();
