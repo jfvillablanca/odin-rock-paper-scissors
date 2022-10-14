9082b8b89b896b17bbbb025c115d672d3d6cab54
Jann Marc Villablanca <jmfvillablanca@gmail.com>
2022-10-14 17:03:14 +0800

    Move eventListener to global scope

--- script.js
+++ script.js
@@ -2,59 +2,6 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
-function getComputerChoice(){
-    let compChoice = Math.floor(Math.random()*3);
-    if (compChoice === 0){
-         return rock; 
-    }
-    else if (compChoice === 1){ 
-         return paper; 
-    }
-    else{
-         return scissors;
-    }
-}
-
-function listenPlayerChoice() {
-    //    while(true) {
-    //        let playerInput = prompt("Rock, Paper, Scissors?");
-    //        playerInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase(); 
-    //        if (playerInput === rock || playerInput === paper || playerInput === scissors){
-    //            return playerInput; 
-    //        }
-    //        else{
-    //            console.log("Wrong input, try again.");
-    //            continue;
-    //        }
-    //    }
-    let playerSelection;
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-
-        button.addEventListener('click', () => {
-            console.log(button.textContent);
-            playerSelection = button.textContent;
-        });
-    });
-    const computerSelection = getComputerChoice();
-
-    let winner = playRound(playerSelection, computerSelection);
-    if (winner === 'Player') {
-        //playerScore++;
-        console.log("You win!");
-        return [1,0];
-    }
-    else if (winner === 'Computer') {
-        computerScore++;
-        console.log("Computer wins!");
-        return [0,1];
-    }
-    else {
-        console.log("It's a tie");
-        return [0,0];
-    }
-}
-
 function playRound(playerSelection, computerSelection) {
     console.log(`Player Choice: ${playerSelection}, Computer Choice: ${computerSelection}`);
     if ((playerSelection === rock && computerSelection === scissors) ||
@@ -71,23 +18,68 @@ function playRound(playerSelection, computerSelection) {
         return 'Tie';
     }
 }
-function game() {
-    let playerScore = 0, computerScore = 0;
-    for (let i = 0; i < 1; i++) {
-        console.log(`Round ${i + 1}, Player Score: ${playerScore}, Computer Score: ${computerScore}`);
-        [playerScoreIncrement,computerScoreIncrement] = listenPlayerChoice();
-        playerScore += playerScoreIncrement;
-        computerScore += computerScoreIncrement;
+function getComputerChoice(){
+    let compChoice = Math.floor(Math.random()*3);
+    if (compChoice === 0){
+         return rock; 
     }
-    if (playerScore > computerScore) {
-        console.log(`For a final score of ${playerScore} against the computer's ${computerScore}, the player wins the match!`);
+    else if (compChoice === 1){ 
+         return paper; 
     }
-    else if (playerScore < computerScore) {
-        console.log(`For a final score of ${computerScore} against the player's ${playerScore}, the computer wins the match!`);
+    else{
+         return scissors;
+    }
+}
+
+
+const game = function(playerChoice){
+    console.log(playerChoice);
+    const playerSelection = playerChoice.textContent;
+    const computerSelection = getComputerChoice();
+
+    let winner = playRound(playerSelection, computerSelection);
+    if (winner === 'Player') {
+        //playerScore++;
+        console.log(`You win! Player Score: ${playerScore}`);
+       // return [1, 0];
+    }
+    else if (winner === 'Computer') {
+        computerScore++;
+        console.log("Computer wins!");
+        //return [0, 1];
     }
     else {
-        console.log(`The match is tied for a final score of ${playerScore} to ${computerScore} to both competitors!`);
+        console.log("It's a tie");
+        //return [0, 0];
     }
+//    for (let i = 0; i < 5; i++) {
+//        console.log(`Round ${i + 1}, Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+//        [playerScoreIncrement,computerScoreIncrement] = listenPlayerChoice();
+//        playerScore += playerScoreIncrement;
+//        computerScore += computerScoreIncrement;
+//    }
+//    if (playerScore > computerScore) {
+//        console.log(`For a final score of ${playerScore} against the computer's ${computerScore}, the player wins the match!`);
+//    }
+//    else if (playerScore < computerScore) {
+//        console.log(`For a final score of ${computerScore} against the player's ${playerScore}, the computer wins the match!`);
+//    }
+//    else {
+//        console.log(`The match is tied for a final score of ${playerScore} to ${computerScore} to both competitors!`);
+//    }
     console.log("Thanks for playing");
 }
-game();
+//game();
+
+let playerScore = 0, computerScore = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+ //   button.addEventListener('click', () => {
+ //       console.log(this);
+ //   //    game;
+ //   });
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', game);
+    });
+});
